43484d4fbf9adcb8654fcfab6c9b437f4b553341
KK <741077081@qq.com>
2016-07-20 20:07:58 +0800

    123

---
+++ hello.py
@@ -0,0 +1,8 @@
+from flask import Flask,render_template
+import os
+app=Flask(__name__)
+@app.route('/',methods=['GET'])
+def index():
+    return render_template('video.html')
+if __name__=='__main__':
+    app.run(debug=True)
